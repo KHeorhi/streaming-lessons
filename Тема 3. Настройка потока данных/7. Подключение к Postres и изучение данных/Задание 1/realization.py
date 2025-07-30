9e2cb258c13157d8c0ea8194f4b0c5906fc87ec9
major_pein <yurykirpichenko@yandex.ru>
2025-07-30 16:15:47 +0000

    подключился к БД

--- Тема 3. Настройка потока данных/7. Подключение к Postres и изучение данных/Задание 1/realization.py
+++ Тема 3. Настройка потока данных/7. Подключение к Postres и изучение данных/Задание 1/realization.py
@@ -1,10 +1,23 @@
 from pyspark.sql import SparkSession
 
-spark = (
-        #
-    )
 
-df = (spark.read
-        #
-        .load())
+spark = (
+        SparkSession.builder.appName('test_name')
+        .config("spark.jars.packages", "org.postgresql:postgresql:42.4.0")
+        .getOrCreate()
+        )
+
+jdbc_option = {
+    'url': 'jdbc:postgresql://rc1a-fswjkpli01zafgjm.mdb.yandexcloud.net:6432/de',
+    'driver': 'org.postgresql.Driver',
+    'dbtable': 'marketing_companies',
+    'user': 'student',
+    'password': 'de-student',
+}
+
+
+df = spark.read.format('jdbc').options(**jdbc_option).load()
 df.count()
+
+
+df.printSchema()
